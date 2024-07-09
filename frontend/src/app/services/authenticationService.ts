3b896ba3bc5d395b58bdb150fd4d721e83929e26
Jonaires777 <airesjonathan123@gmail.com>
2024-07-09 09:41:58 -0300

    feat: add authenticationService

---
+++ frontend/src/app/services/authenticationService.ts
@@ -0,0 +1,51 @@
+import apiProvider from "../providers/apiProvider";
+
+type AuthenticationResponse = {
+    token: string;
+}
+
+type AuthenticationRequest = {
+    email: string;
+    password: string;
+}
+
+type SignUpResponse = {
+    token: string;
+}
+
+type SignUpRequest = {
+    username: string;
+    email: string;
+    password: string;
+}
+
+const login = async (email: string, password: string): Promise<AuthenticationResponse> => {
+    try {
+        const response = await apiProvider.post<AuthenticationResponse, AuthenticationRequest>("/login", {
+            email,
+            password,
+        })
+        return response
+    } catch (error) {
+        throw error
+    }
+}
+
+const signup = async (username: string, email: string, password: string): Promise<SignUpResponse> => {
+    try {
+        const response = await apiProvider.post<SignUpResponse, SignUpRequest>("/signup", {
+            email,
+            password,
+            username
+        })
+        return response
+    } catch (error) {
+        throw error
+    }
+}
+
+const authenticationService = {
+    login, signup
+}
+
+export default authenticationService
