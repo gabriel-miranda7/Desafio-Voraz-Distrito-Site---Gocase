d8d83471c3c406bdf1041211b15cee8a23404a85
Gabriel Miranda <gabrielsmbr@gmail.com>
2024-07-12 22:06:54 -0300

    major fix: creating user resulting in null scores

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -3,8 +3,18 @@ class User < ApplicationRecord
     has_secure_password
     validates :email, presence: true, uniqueness: true
 
+    before_create :initialize_scores
+
     def create_user_profile
         # Create vector that represents user profile
-        @user_profile = Vector[self.masculine_cloth_score, self.feminine_cloth_score, self.electronic_score, self.jewelry_score]
+        @user_profile = Vector[self.masculine_cloth_score || 2, self.feminine_cloth_score || 2, self.electronic_score || 2, self.jewelry_score ||2]
     end
+
+    private
+    def initialize_scores
+        self.masculine_cloth_score ||= 2
+        self.feminine_cloth_score ||= 2
+        self.electronic_score ||= 2
+        self.jewelry_score ||= 2
+      end
 end
