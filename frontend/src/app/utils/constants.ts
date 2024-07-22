015684c3a42117ca7cd9eade209b8e45906e9de2
Gabriel da Silveira Miranda <gabrielsmbr@gmail.com>
2024-07-21 23:42:38 -0300

    fix: changed BASE_API_URL

--- frontend/src/app/utils/constants.ts
+++ frontend/src/app/utils/constants.ts
@@ -1,2 +1,2 @@
 export const BASE_API_URL =
-  process.env.NEXT_PUBLIC_BASE_API_URL || "http://localhost:5000";
+  process.env.NEXT_PUBLIC_BASE_API_URL || "https://desafio-voraz-distrito-site-gocase-1.onrender.com";
